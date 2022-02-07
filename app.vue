e825447f7e2459c254b1b89e7a5828d8b7c4bcf6
Joachim <joachim.dev@pm.me>
2022-02-07 17:46:06 +0100

    changes

--- app.vue
+++ app.vue
@@ -1,23 +1,46 @@
+<script setup>
+const openMail = () => {
+  window.open('mailto:poststelle@eltville-gymnasium.de', 'self');
+}
+</script>
+
 <template>
-  <div class="main main-container">
+  <div class="main main-container center">
     <pCard>
       <template #title>
         Diese Seite wird momentan überarbeitet!
       </template>
       <template #content >
-        <pMessage severity="warn" :closable="false">Wir sind bald wieder für Sie da!</pMessage>
+        
+        <pCard>
+          <template #header>
+            <img src="/images/gym-eltville-logo.png"/>
+          </template>
+          <template #content>
+            <b>Für mehr Information:</b>
+            <br>
+            <br>
+            Email: <pBadge size="large" value="poststelle@eltville-gymnasium.de"></pBadge>
+            <br>
+            <br>
+            Anschrift: <pBadge size="large" value="Wiesweg 7, 65343 Eltville am Rhein"></pBadge>
+            <pMessage severity="warn" :closable="false">Wir sind bald wieder für Sie da!</pMessage>
+          </template>
+        </pCard>
       </template>
     </pCard>
   </div>
-  <div class="main main-footer">
-    
-  </div>
+  <pSplitter class="main main-footer">
+    <pSplitterPanel class="center">
+      <pButton icon="pi pi-envelope" label="Email: poststelle@eltville-gymnasium.de" @click="openMail()"></pButton>
+    </pSplitterPanel>
+    <pSplitterPanel class="center space-evenly">
+      <img class="contain-element" src="/images/gym-eltville-logo.png"/>
+    </pSplitterPanel>
+  </pSplitter>
 </template>
 
 <style>
-.pad-right {
-  margin-right: 5px;
-}
 .main {
   position: relative;
   width: 90vw;
@@ -25,10 +48,30 @@
 .main-container {
   z-index: 1;
   height: 75vh;
+}
+
+.main-footer {
+  z-index: 1;
+  height: 10vh;
+  display: flex;
+  align-items: stretch;
+  justify-content: stretch;
+}
+
+.center {
   display: flex;
   align-items: center;
   justify-content: center;
 }
+
+.space-evenly {
+  justify-content: space-evenly;
+}
+
+.contain-element {
+  max-height: 80%; 
+}
+
 body {
   margin: 0;
   padding: 5vh 5vw;
@@ -56,8 +99,5 @@ body::after {
   background-blend-mode: multiply;
 }
 
-.main-footer {
-
-}
 
 </style>
